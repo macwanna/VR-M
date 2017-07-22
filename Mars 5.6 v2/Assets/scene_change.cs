ff06af6aea53dbea4a5249369398cfb30a6ec0a9
Macwana <priyummacwana@gmail.com>
2017-07-22 16:18:42 +0530

    Files
    All assets and necessary stuff. Some of them will need to go eventually.

---
+++ Mars 5.6 v2/Assets/scene_change.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_change : MonoBehaviour {
+	public void go()
+	{
+		SceneManager.LoadScene ("entry scene on sofa");
+	}
+}
